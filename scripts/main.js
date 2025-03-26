9d11966c25c29367a033e347c957cbef23361df3
Mutairu-Lawal <thebillionnairadream@gmail.com>
2025-03-26 15:24:46 +0000

    Add card title styles and implement main functionality for quiz navigation

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,30 @@
-const quizLinks = document.querySelectorAll(".js-subject-link");
-
-quizLinks.forEach((link) => {
-  link.addEventListener("click", (e) => {
-    openQuizPage(e, link.querySelector("h5").innerHTML);
-  });
-});
-
-document.querySelectorAll(".js-general-link").forEach((link) => {
-  link.addEventListener("click", (e) => {
-    openQuizPage(e);
-  });
-});
-
-function openQuizPage(e, quizType = "General knowledge") {
-  e.preventDefault();
-  let newQuestionName = quizType;
-  assignValue(newQuestionName);
-  saveData();
-  open("quiz-page.html", "_self");
-}
-
-function assignValue(valueName) {
-  newQuestion = valueName;
-}
-
-function saveData() {
-  localStorage.setItem("quizWhiz-user-data", newQuestion);
-}
-
+"use strict";
+// select all links
+const quizLinks = document.querySelectorAll('.js-subject-link');
+const thisYear = document.querySelector('#current-year');
+// set the year
 const currentYear = new Date().getFullYear();
-document.getElementById("current-year").textContent = currentYear;
+thisYear.textContent = currentYear.toString();
+// add eventlistener for page routing
+quizLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        var _a;
+        const quizName = (_a = link.querySelector('h1')) === null || _a === void 0 ? void 0 : _a.innerHTML;
+        if (quizName)
+            openQuizPage(e, quizName);
+    });
+});
+document.querySelectorAll('.js-general-link').forEach((link) => {
+    link.addEventListener('click', (e) => {
+        openQuizPage(e);
+    });
+});
+function openQuizPage(e, quizType = 'General knowledge') {
+    e.preventDefault();
+    let newQuestionName = quizType;
+    saveData(newQuestionName);
+    open('quiz-page.html', '_self');
+}
+function saveData(value) {
+    localStorage.setItem('quizWhiz-user-data', value);
+}
