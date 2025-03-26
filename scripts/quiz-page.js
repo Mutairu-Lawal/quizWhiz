788678854b57cb85943f19a9045c94284ce92b9b
Mutairu-Lawal <thebillionnairadream@gmail.com>
2025-03-26 11:52:13 +0000

    create quiz page and added back all odd scripts

--- scripts/quiz-page.js
+++ scripts/quiz-page.js
@@ -1,4 +1,4 @@
-import { comments } from '../utils/comments.js';
+import { comments } from '../old-data/utils/comments.js';
 import {
   hardQuestions,
   mathsQuestions,
@@ -8,7 +8,7 @@ import {
   sports,
 } from '../data/questions.js';
 
-import { stopTimer, checkTime, startTimer, clearData } from '../utils/time.js';
+import { stopTimer, checkTime, startTimer, clearData } from '../old-data/utils/time.js';
 
 const backBtn = document.querySelector('.js-back-btn');
 const nextBtn = document.querySelector('.js-next-btn');
